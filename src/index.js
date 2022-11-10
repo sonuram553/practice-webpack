9bf00418f7e4358a8d4105edb2ee607317447f01
Sonu Ram <sonuram6960@gmail.com>
2022-11-11 02:34:06 +0530

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import _ from "lodash";
+import "./style.css";
+import Tree from "./tree.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div.
+  const img = new Image();
+  img.src = Tree;
+  element.appendChild(img);
+
+  console.log(Data);
+  console.log(Notes);
+
   return element;
 }
 
