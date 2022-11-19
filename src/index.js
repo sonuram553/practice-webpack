77e5b82029cfa6704b0847ddc729607d2ad22dfb
Sonu Ram <sonuram6960@gmail.com>
2022-11-19 15:05:08 +0530

    Caching

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-function getComponent() {
-  return import("lodash")
-    .then(({ default: _ }) => {
-      const element = document.createElement("div");
+import _ from "lodash";
+import Print from "./print";
 
-      element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-      return element;
-    })
-    .catch((err) => "An error occurred while loading the component");
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.onclick = Print.bind(null, "Hello webpack!");
+  return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,28 @@ module.exports = {
     static: "./dist",
   },
 
-  entry: { index: "./src/index.js" },
+  entry: { main: "./src/index.js" },
 
-  plugins: [new HtmlWebpackPlugin({ title: "Development" })],
+  plugins: [new HtmlWebpackPlugin({ title: "Caching" })],
 
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
     publicPath: "/",
+  },
+
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 };
